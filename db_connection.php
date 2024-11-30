bbaff7e205cc013d9c9f3c380decb3cbf085a2e3
mymuna-rahman <mymunarahman03@gmail.com>
2024-12-01 00:48:46 +0600

    Create db_connection.php

---
+++ db_connection.php
@@ -0,0 +1,8 @@
+<?php
+$servername = "localhost"; 
+$username = "root";       // Default username for XAMPP
+$password = "";           // Default password is blank in XAMPP
+$dbname = "greengrid"; 
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
